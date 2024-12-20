7d2899db6ffcdf220640cd8ed6bb5bef79531963
RoseJam2023 <rddrummond42@gmail.com>
2024-12-20 10:56:13 -0600

    G: created updateable and drawable groups and converted game loop to utilize groups

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ def main():
     pygame.init
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
     dt = 0
+    
     
 
     while True:
@@ -16,9 +24,15 @@ def main():
                 return
 
         screen.fill((0,0,0), rect= None, special_flags= 0)
-        player.update(dt)
-        player.draw(screen)
+
+        for object in updatable:
+            object.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
