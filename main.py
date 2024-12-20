a15023149a4bdef9b9e9a19665b92dda037aa9bd
RoseJam2023 <rddrummond42@gmail.com>
2024-12-20 12:18:54 -0600

    H: created asteroid.py and asteroidfield.py, added logic to main to create asteroid field object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 
 def main():
     pygame.init
@@ -9,14 +10,16 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
-    
+    asteroid_field = AsteroidField()
+
     dt = 0
-    
-    
 
     while True:
         for event in pygame.event.get():
