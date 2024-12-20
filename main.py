3d69b151b8d2443e2dc8dc252df6ae0cb65d6f4c
RoseJam2023 <rddrummond42@gmail.com>
2024-12-19 19:25:19 -0600

    added player radius to constants.py, created player.py, added several import lines to multiple files, added to main to draw triangle to screen

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +16,7 @@ def main():
                 return
 
         screen.fill((0,0,0), rect= None, special_flags= 0)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
