1f3910d9c5136e8a5d7f935b382a511f64a858b4
RoseJam2023 <rddrummond42@gmail.com>
2024-12-20 13:04:59 -0600

    J: created shot.py class, updated player.py to allow player to shoot, added contants related to shooting, added container for shots to main.py

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
+import pygame
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -31,7 +33,16 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        new_shot = Shot(self.position.x, self.position.y)
+        starting_vector = pygame.Vector2(0,1)
+
+        new_shot.velocity = (starting_vector.rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+    
